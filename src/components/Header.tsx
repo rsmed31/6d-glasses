034e3b18f615b183f66f5746c6907327fa3a801a
rsmed31 <112734485+rsmed31@users.noreply.github.com>
2022-12-12 23:57:23 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,17 +43,13 @@ export function Header({ height }: HeaderProps): JSX.Element {
       <div class="menu-wrap">
          <input type="checkbox" class="toggler">
            <div class="hamburger"><div></div></div>
-          <div class="menu">
-          <div>
-            <div>
-             <ul>
+          <div class="menu"><div>
+            <div><ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about_us">About-us</a></li>
-            </ul>
-            </div>
-          </div>
-        </div>
-        </div>
+            </ul></div>
+          </div></div>
+      </div>
     </HeaderContainer>
   );
 }
